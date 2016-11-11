ee883d0160f5fe1d1ec18ae482f5f631a29fe170
Andrew Runciman <=>
2016-11-11 10:22:40 +0000

    Added Sequelize playground

--- playground/basic-sql.js
+++ playground/basic-sql.js
@@ -6,19 +6,71 @@ var sequelize = new Sequelize(undefined,undefined,undefined,{
 
 var Todo = sequelize.define('todo',{
     description:{
-        type:Sequelize.STRING
+        type:Sequelize.STRING,
+        allowNull:false,
+        validate:{
+            len:[1,250]
+        }
     },
     completed:{
-        type:Sequelize.BOOLEAN
+        type:Sequelize.BOOLEAN,
+        allowNull:false,
+        defaultValue:false
     }
 });
 
 sequelize.sync().then(function(){
-    console.log('Everything is synced');
     Todo.create({
-        description:'walking the cat',
-        completed:false
+        description:'play the piano'
     }).then(function(todo){
-        console.log(todo);
+        return Todo.create({
+            description:'eat chips'
+        });
+    }).then(function(todo){
+        return Todo.findAll({
+            where:{
+                id:[11,12,13,14]
+            }
+        });
+    }).then(function(todos){
+        if(todos){
+            todos.forEach(function(todo){
+                console.log(todo.toJSON());
+            })
+        }else{
+            console.log("not found")
+        }
+    },function(err){
+        console.log(err)
     });
+},function(err){
+    console.log(err);
 });
+
+// sequelize.sync().then(function(){
+//     console.log('Everything is synced');
+//     Todo.create({
+//         description:'take out trash',
+//     }).then(function(todo){
+//         return Todo.create({
+//             description:"clean office"
+//         });
+//     }).then(function(){
+//         return Todo.findAll({
+//             where:{
+//                 completed:false
+//             }
+//         });
+//     }).then(function(todos){
+//         if(todos){
+//             todos.forEach(function(todo){
+//                 console.log(todo.toJSON());
+//             });
+            
+//         }else{
+//             console.log('cant find id=1')
+//         }
+//     }).catch(function(err){
+//         console.log(err);
+//     });
+// });
--- playground/example-promise.js
+++ playground/example-promise.js
@@ -9,17 +9,20 @@ doWork('important work',function(data){
 
 function doWorkPromise(data){
     return new Promise(function(res,rej){
-        // setTimeout(function(){
-        //     res('everything worked');
-        // },2000);
         
-        rej({
-            error:'fucking broke!'
-        })
+        if(data){
+            setTimeout(function(){
+                res('everything worked');
+            } ,2000);
+        }else{
+            rej({
+                error:'fucking broke!'
+            })
+        }
     });
 }
 
-doWorkPromise('some data').then(function(data){
+doWorkPromise(true).then(function(data){
     console.log(data);
 },function(error){
     console.log(error.error);
