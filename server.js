9d7e9ef2030b9a723ab4c5d73147e29a5d8649de
Andrew Runciman <=>
2016-11-11 13:06:42 +0000

    cleaned out code GET TODO now working

--- server.js
+++ server.js
@@ -2,29 +2,10 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
 var db = require('./db.js');
-
 var app = express();
 var PORT = process.env.PORT || 3000; //Works for Horoko
 
-
 app.use(bodyParser.json());
-
-//model + collection
-var todos = [{
-	id: 1,
-	description: 'pick up car',
-	completed: false
-}, {
-	id: 2,
-	description: 'go to supermarket',
-	completed: false
-}, {
-	id: 3,
-	description: 'make tea',
-	completed: true
-}];
-
-var todoNextId = 4;
 
 app.get('/', function (req, res) {
 	res.send('Todo API Root');
@@ -32,34 +13,38 @@ app.get('/', function (req, res) {
 
 //GET /todos
 app.get('/todos', function (req, res) {
-	var queryParams = req.query;
-	var filteredTodo = todos;
-
-	if (queryParams.hasOwnProperty('completed') && queryParams.completed === "true") {
-		//do this as the queryparam is string and needs to be boolean
-		filteredTodo = _.where(filteredTodo, { completed: true });
-	} else if (queryParams.hasOwnProperty('completed') && queryParams.completed === "false") {
-		filteredTodo = _.where(filteredTodo, { completed: false });
+	var query = req.query;
+	var where = {};
+	if (query.hasOwnProperty('completed') && query.completed === "true"){
+		where.completed = true;
+	} else if (query.hasOwnProperty('completed') && query.completed === "false"){
+		where.completed = false;
 	}
-
-	if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
-		filteredTodo = _.filter(filteredTodo, function (todo) {
-			return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) >= 0
-		});
+	if (query.hasOwnProperty('q') && query.q.length > 0) {
+		where.description = {
+			$like:'%' + query.q + '%'
+		};
 	}
-
-	res.json(filteredTodo);
+	db.todo.findAll({where:where}).then(function(todos){
+		res.json(todos);
+	}).catch(function(err){
+		res.status(500).send(err);
+	});;
 });
 
 //GET /todo/:id
 app.get('/todos/:id', function (req, res) {
 	var index = parseInt(req.params.id);
-	var matchedTodo = _.findWhere(todos, { id: index });
-	if (matchedTodo) {
-		res.json(matchedTodo);
-	} else {
-		res.status(404).send();
-	}
+	db.todo.findById(index).then(function(todo){
+		if(todo){
+			res.json(todo.toJSON());
+		}else{
+			res.status(404).send();
+		}
+	}).catch(function(err){
+		res.status(500).send(err);
+	})
+
 });
 
 // POST /todos/
@@ -70,10 +55,6 @@ app.post('/todos', function (req, res) {
 		return res.status('400').send("Invalid data");
 	}
 
-	console.log('about to add new todo!')
-	//use the value if it was set and is boolean otherwise default to false.
-	//var _completed = (body.completed && _.isBoolean(body.completed))?body.completed:false;
-
 	db.todo.create({
 		description: body.description,
 		completed: body.completed || false
@@ -83,64 +64,55 @@ app.post('/todos', function (req, res) {
 		res.json(err);
 	});
 
-	// if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-	// 	return res.status(400).send(); //400 bad data provided
-	// }
-
-	// body.id = todoNextId++ //sets the body id to the current num then increments
-	// body.description = body.description.trim();
-
-	// todos.push(body);
-	// res.send(body);
 });
 
 //DELETE /todo/:id
 app.delete('/todos/:id', function (req, res) {
-	var index = parseInt(req.params.id);
-	//var item = _.find(todos,function(body){return body.id === index});
-	var item = _.findWhere(todos, { id: index });
+	// var index = parseInt(req.params.id);
+	// //var item = _.find(todos,function(body){return body.id === index});
+	// var item = _.findWhere(todos, { id: index });
 
-	if (item) {
-		//var itemPos = _.indexOf(todos,item)
-		//todos.splice(itemPos,1);
-		todos = _.without(todos, item); //refactored
-		res.send(item);
-	} else {
-		return res.status(404).send();
-	}
+	// if (item) {
+	// 	//var itemPos = _.indexOf(todos,item)
+	// 	//todos.splice(itemPos,1);
+	// 	todos = _.without(todos, item); //refactored
+	// 	res.send(item);
+	// } else {
+	// 	return res.status(404).send();
+	// }
 });
 
 app.put('/todos/:id', function (req, res) {
 
 	var body = _.pick(req.body, 'description', 'completed'); //just gets the fields we are interested in 
-	var validAttributes = {};
-	var index = parseInt(req.params.id);
-	var matchedTodo = _.findWhere(todos, { id: index });
+	// var validAttributes = {};
+	// var index = parseInt(req.params.id);
+	// var matchedTodo = _.findWhere(todos, { id: index });
 
-	if (!matchedTodo) {
-		return res.status(404).send();
-		//404 not found
-	}
+	// if (!matchedTodo) {
+	// 	return res.status(404).send();
+	// 	//404 not found
+	// }
 
-	if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
-		validAttributes.completed = body.completed;
-	} else if (body.hasOwnProperty('completed')) {
-		return res.status(400).send();
-		//400 bad data
-	}
-	//seems to be open to SQL injection / Javascript injection
+	// if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+	// 	validAttributes.completed = body.completed;
+	// } else if (body.hasOwnProperty('completed')) {
+	// 	return res.status(400).send();
+	// 	//400 bad data
+	// }
+	// //seems to be open to SQL injection / Javascript injection
 
-	if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
-		validAttributes.description = body.description;
-	} else if (body.hasOwnProperty('description')) {
-		return res.status(400).send();
-	}
+	// if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
+	// 	validAttributes.description = body.description;
+	// } else if (body.hasOwnProperty('description')) {
+	// 	return res.status(400).send();
+	// }
 
-	//this works as object passed by reference.
-	_.extend(matchedTodo, validAttributes);
+	// //this works as object passed by reference.
+	// _.extend(matchedTodo, validAttributes);
 
-	//automatically sends 200
-	res.json(matchedTodo);
+	// //automatically sends 200
+	// res.json(matchedTodo);
 
 });
 
